29bfd7a6dc5f313e9b1bddbab20ca2a2e9f96022
ramzi-omari <ramziomari1995@gmail.com>
2021-01-08 22:36:59 +0100

    done

--- weatherapp.py
+++ weatherapp.py
@@ -14,6 +14,31 @@ img = Image.open("./index.png")
 img = img.resize((150, 150))
 img = ImageTk.PhotoImage(img)
 
-LocationLabel = Label(master, font=("Calibri bold", 20), bg="white")
-LocationLabel.grid(row=0, sticky="N", padx=100)
+
+def getWeather():
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    location = soup.find(
+        "h1", class_="CurrentConditions--location--1Ayv3").text
+    temperature = soup.find(
+        'span', class_="CurrentConditions--tempValue--3KcTQ").text
+    weatherPrediction = soup.find(
+        'div', class_="CurrentConditions--phraseValue--2xXSr").text
+    locationLabel.config(text=location)
+    temperatureLabel.config(text=temperature)
+    weatherPredictionLabel.config(text=weatherPrediction)
+
+    temperatureLabel.after(1000, getWeather)
+    master.update()
+
+
+locationLabel = Label(master, font=("Calibri bold", 20), bg="white")
+locationLabel.grid(row=0, sticky="N", padx=100)
+temperatureLabel = Label(master, font=("Calibri bold", 70), bg="white")
+temperatureLabel.grid(row=1, sticky="W", padx=40)
+Label(master, image=img, bg="white").grid(row=1, sticky="E")
+weatherPredictionLabel = Label(master, font=("Calibri bold", 15), bg="white")
+weatherPredictionLabel.grid(row=2, sticky="W", padx=40)
+
+getWeather()
 master.mainloop()
